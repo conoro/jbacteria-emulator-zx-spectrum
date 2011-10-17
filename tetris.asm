7e9607391cad049de16fd5469b18e3cd0cccaeb9
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2011-10-17 09:05:28 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@104 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- tetris.asm
+++ tetris.asm
@@ -2,8 +2,8 @@
         org   $5ccb
 ini     ld    b, 23
         ld    a, $11
-        db    $d7, $c0, $37, $0e, $8f, $39, $96 ;OVER USR 7 ($5ccb)
-        db    $3a ;xor   a
+        db    $d7, $c0, $37, $0e, $8f, $39, $96 ; BEEP USR 7 ($5ccb)
+        db    $3a               ;xor   a
 tet1    rst   $10
         ld    a, 13
         ld    (time-1), a
@@ -11,8 +11,7 @@ tet1    rst   $10
 ini2    ld    a, 249
         djnz  tet1
         ld    hl, $0110
-        ld    ($5c09), hl       ; loaded to REPDEL and REPPER.
-
+        ld    ($5c09), hl       ;REPDEL, REPPER
 bpgs    ld    a, ($5c78)
 resi    sub   $f9
         jr    c, resi
@@ -23,18 +22,18 @@ resi    sub   $f9
         add   a, a
         add   a, l
         ld    (opc0+1), a
-        ld    de, 6  | 2<<5 | $5800
+        ld    de, 6 | 2<<5 | $5800
+        ld    c, d
         push  de
         ld    h, $5d
-        ld    l, (hl)
         jr    tttt
-        db    %11101000 ;7l
-        db    %01100011 ;z
-        db    %01100110 ;cu
-        db    %00110110 ;s
-        db    %01001110 ;t
-        db    %00001111 ;ba
-        db    %10001110; 0010 ;es
+        db    %11101000
+        db    %01100011
+        db    %01100110
+        db    %00110110
+        db    %01001110
+        db    %00001111
+        db    %10001110
 disi    ld    de, 31 | 22<<5 | $5800
 dis2    ld    a, $38+10
         ld    hl, $ffe0
@@ -55,35 +54,28 @@ dis2    ld    a, $38+10
         ld    l, time-1
         dec   (hl)
         jr    disi-1
-
-tttt    ld    h, b
+tttt    ld    l, (hl)
+        ld    h, b
         add   hl, hl
         add   hl, hl
         add   hl, hl
         add   hl, hl
         push  hl
-        ld    c, d
-
-lejo    ld    ix, opc2
+lejo    ld    ixl, opc2
         call  pint
-        jr    z, delg     ;si no final, bien
+        jr    z, delg
         pop   hl
         pop   de
         ld    sp, $ff40
         bit   2, c
         jr    z, ini
         inc   c
-; compuebo final
-
-delg    ld    ixl, opci
-        dec   (ix)
-        ld    ixl, opc0
-        call  pint        ; pinta pieza
-        ld    ixl, opci
-        inc   (ix)
+delg    ld    ixl, opc0
+        dec   (ix+opci-opc0)
+        call  pint              ; pinta pieza
+        inc   (ix+opci-opc0)
 teal    bit   1, c
         jr    nz, disi
-; compruebo linea hecha
 ted     ld    a, ($5c78)
         sub   0
 time    sub   0
@@ -95,9 +87,9 @@ salt    push  de
         push  hl
         res   5, (iy+1)
         ld    ixl, opc1
-        ex    af, af'
-        call  pint        ; borra pieza
-        ex    af, af'
+        ex    af, af
+        call  pint              ; borra pieza
+        ex    af, af
         cp    'p'
         jr    nz, nrigh
         inc   e
@@ -120,7 +112,7 @@ nfina   djnz  akir
         pop   hl
         rr    h
         rr    l
-        db    $ca; jr    akia
+        db    $ca               ; jr    akia
 rota    ld    c, a
         inc   a
 akia    ld    b, 4
@@ -147,21 +139,11 @@ pint2   add   hl, hl
 pint3   call  $03f4
 pint4   dec   de
         djnz  pint2
-        ld    b, 28
+        ld    b, 32-4
 sum28   dec   de
         djnz  sum28
         dec   c
         jr    nz, pint1
-pint5   pop   hl
-        pop   de
-        pop   bc
-        ret
-
-opc2    ex    de, hl
-        add   a, (hl)
-        ex    de, hl
-        ret   z
-        pop   de
         jr    pint5
 
 opc0    ld    a, 0
@@ -174,7 +156,18 @@ opc1    ld    (de), a
 opci    dec   (hl)
         pop   hl
         ret
+
+opc2    ex    de, hl
+        add   a, (hl)
+        ex    de, hl
+        ret   z
+        pop   de
+pint5   pop   hl
+        pop   de
+        pop   bc
+        ret
 fin
+
 /*<?php require 'zx.inc.php';
   exec('sjasmplus tetris.asm');
   $in= file_get_contents('tetris.bin');
