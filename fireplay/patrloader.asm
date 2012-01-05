5dba3f85cd13bb82e8008681b63607a4f4d5bb53
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2012-01-05 15:37:44 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@152 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- fireplay/patrloader.asm
+++ fireplay/patrloader.asm
@@ -4,14 +4,18 @@
         DEFINE  L2CB3 $2cb3
         DEFINE  L2DE3 $2de3
         DEFINE  L1F4F $1f4f
+        DEFINE  SNAP1 $0000
+        DEFINE  LEEBI $0000
+        DEFINE  enram
+;        DEFINE  sinborde
 
         output  "patrloader.bin"
-        org     $b86e-$55
-        ld      de, $b86e
+        org     $b4bf-$55
+        ld      de, $b4bf
         nop
         db      $de, $c0, $37, $0e, $8f, $39, $96 ;OVER USR 7 ($5ccb)
         ld      hl, $5ccb+$55
-        ld      bc, L3C02-L386E
+        ld      bc, L37FF+3-L34BF
         ldir
 recar   ld      de, $1b00
         ld      hl, $4000
@@ -45,8 +49,9 @@ L0556:  push    hl
         IN      A,($FE)         ; read the ear state - bit 6.
         RRA                     ; rotate to bit 5.
         AND     $20             ; isolate this bit.
-        CALL    L3A0D
+        CALL    L360D
 loader  include ultra.asm
+
 
 /*<?php require 'zx.inc.php';
   generate_basic('patrloader')?>*/
