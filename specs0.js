5ec70e5baabe708a39c747bee78d442432a566ad
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2012-07-15 16:39:47 +0000

    arreglado scroll hardware
    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@213 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- specs0.js
+++ specs0.js
@@ -1,158 +1,122 @@
 function paintNormal(){
   mix= miy= 32;
   max= may= t= -1;
-  if( scrl&7 ){
+  if( scrl & 136 ){
     while( ++t < 0x300 )
-      if( t&0x1f )
-        for ( col= m[t+0x5800]
-            , bk= pal[col    & 7
-                    | col>>3 & 8]
-            , fo= pal[col>>3 & 15]
-            , dx= t & 0x1f
-            , dy= t>>5
-            , o= ( dy << 13 | t<<5 & 0x3ff ) - (scrl<<2)
-            , u=  0x4000
-                | t    & 0xff 
-                | t<<3 & 0x1800
-            ; ! ( 0x1800
-                & ( u ^ t<<3 )
-                )
-            ; u+= 0x100
-            , o+= 0x400)
-          if( k=  col>>7
-                & flash>>4
-                  ? ~m[u]
-                  : m[u]
-            , vm[u-0x4000] != (col | k<<8) ){
-            vm[u-0x4000]= col | k<<8;
-            if(dx-scrl<mix)
-              mix= dx-scrl;
-            else
-              dx>max && (max= dx);
-            if(dy < miy)
-              miy= dy;
-            else
-              dy>may && (may= dy);
-            if( k&128 )
-              eld[o  ]= bk[0],
-              eld[o+1]= bk[1],
-              eld[o+2]= bk[2];
-            else
-              eld[o  ]= fo[0],
-              eld[o+1]= fo[1],
-              eld[o+2]= fo[2];
-            if( k&64 )
-              eld[o+4]= bk[0],
-              eld[o+5]= bk[1],
-              eld[o+6]= bk[2];
-            else
-              eld[o+4]= fo[0],
-              eld[o+5]= fo[1],
-              eld[o+6]= fo[2];
-            if( k&32 )
-              eld[o+8 ]= bk[0],
-              eld[o+9 ]= bk[1],
-              eld[o+10]= bk[2];
-            else
-              eld[o+8 ]= fo[0],
-              eld[o+9 ]= fo[1],
-              eld[o+10]= fo[2];
-            if( k&16 )
-              eld[o+12]= bk[0],
-              eld[o+13]= bk[1],
-              eld[o+14]= bk[2];
-            else
-              eld[o+12]= fo[0],
-              eld[o+13]= fo[1],
-              eld[o+14]= fo[2];
-            if( k&8 )
-              eld[o+16]= bk[0],
-              eld[o+17]= bk[1],
-              eld[o+18]= bk[2];
-            else
-              eld[o+16]= fo[0],
-              eld[o+17]= fo[1],
-              eld[o+18]= fo[2];
-            if( k&4 )
-              eld[o+20]= bk[0],
-              eld[o+21]= bk[1],
-              eld[o+22]= bk[2];
-            else
-              eld[o+20]= fo[0],
-              eld[o+21]= fo[1],
-              eld[o+22]= fo[2];
-            if( k&2 )
-              eld[o+24]= bk[0],
-              eld[o+25]= bk[1],
-              eld[o+26]= bk[2];
-            else
-              eld[o+24]= fo[0],
-              eld[o+25]= fo[1],
-              eld[o+26]= fo[2];
-            if( k&1 )
-              eld[o+28]= bk[0],
-              eld[o+29]= bk[1],
-              eld[o+30]= bk[2];
-            else
-              eld[o+28]= fo[0],
-              eld[o+29]= fo[1],
-              eld[o+30]= fo[2];
-          }
-    t= -1;
-    o= -scrl*4;
-    while( ++t < 0x18 )
-      for ( col= m[t<<5 | 0x5800]
+      for ( col= m[t+0x5800]
           , bk= pal[col    & 7
                   | col>>3 & 8]
           , fo= pal[col>>3 & 15]
-          , col2= m[t | 0x5b20]
-          , bk2= pal[col2    & 7
-                   | col2>>3 & 8]
-          , fo2= pal[col2>>3 & 15]
+          , dx= t & 0x1f
+          , dy= t>>5
+          , o=  ( dy << 13 | t<<5 & 0x3ff )
+              - ( scrl <<6 & 7168 )
+              - ( scrl <<2 & 28 )
           , u=  0x4000
-              | t<<5 & 0xff 
-              | t<<8 & 0x1800
+              | t    & 0xff 
+              | t<<3 & 0x1800
           ; ! ( 0x1800
-              & ( u ^ t<<8 )
+              & ( u ^ t<<3 )
               )
           ; u+= 0x100
-          , o+= 0x3e0)
+          , o+= 0x400)
         if( k=  col>>7
               & flash>>4
                 ? ~m[u]
                 : m[u]
           , vm[u-0x4000] != (col | k<<8) ){
           vm[u-0x4000]= col | k<<8;
-          mix= 0;
-          max= 31;
-          if(t < miy)
-            miy= t;
+          if(dx-scrl<mix)
+            mix= dx-scrl;
           else
-            t>may && (may= t);
-          for ( v= 0; v<scrl; v++, o+=4 )
-            if( k&(128>>v) )
-              eld[o+1024]= bk2[0],
-              eld[o+1025]= bk2[1],
-              eld[o+1026]= bk2[2];
-            else
-              eld[o+1024]= fo2[0],
-              eld[o+1025]= fo2[1],
-              eld[o+1026]= fo2[2];
-          while( v<8 ){
-            if( k&(128>>v) )
-              eld[o  ]= bk[0],
-              eld[o+1]= bk[1],
-              eld[o+2]= bk[2];
-            else
-              eld[o  ]= fo[0],
-              eld[o+1]= fo[1],
-              eld[o+2]= fo[2];
-            v++;
-            o+= 4;
-          }
+            dx>max && (max= dx);
+          if(dy < miy)
+            miy= dy;
+          else
+            dy>may && (may= dy);
+          if( k&128 )
+            eld[o  ]= bk[0],
+            eld[o+1]= bk[1],
+            eld[o+2]= bk[2];
+          else
+            eld[o  ]= fo[0],
+            eld[o+1]= fo[1],
+            eld[o+2]= fo[2];
+          if( k&64 )
+            eld[o+4]= bk[0],
+            eld[o+5]= bk[1],
+            eld[o+6]= bk[2];
+          else
+            eld[o+4]= fo[0],
+            eld[o+5]= fo[1],
+            eld[o+6]= fo[2];
+          if( k&32 )
+            eld[o+8 ]= bk[0],
+            eld[o+9 ]= bk[1],
+            eld[o+10]= bk[2];
+          else
+            eld[o+8 ]= fo[0],
+            eld[o+9 ]= fo[1],
+            eld[o+10]= fo[2];
+          if( k&16 )
+            eld[o+12]= bk[0],
+            eld[o+13]= bk[1],
+            eld[o+14]= bk[2];
+          else
+            eld[o+12]= fo[0],
+            eld[o+13]= fo[1],
+            eld[o+14]= fo[2];
+          if( k&8 )
+            eld[o+16]= bk[0],
+            eld[o+17]= bk[1],
+            eld[o+18]= bk[2];
+          else
+            eld[o+16]= fo[0],
+            eld[o+17]= fo[1],
+            eld[o+18]= fo[2];
+          if( k&4 )
+            eld[o+20]= bk[0],
+            eld[o+21]= bk[1],
+            eld[o+22]= bk[2];
+          else
+            eld[o+20]= fo[0],
+            eld[o+21]= fo[1],
+            eld[o+22]= fo[2];
+          if( k&2 )
+            eld[o+24]= bk[0],
+            eld[o+25]= bk[1],
+            eld[o+26]= bk[2];
+          else
+            eld[o+24]= fo[0],
+            eld[o+25]= fo[1],
+            eld[o+26]= fo[2];
+          if( k&1 )
+            eld[o+28]= bk[0],
+            eld[o+29]= bk[1],
+            eld[o+30]= bk[2];
+          else
+            eld[o+28]= fo[0],
+            eld[o+29]= fo[1],
+            eld[o+30]= fo[2];
         }
-        else
-          o+= 32;
+    bk= pal[bor&7];
+    if( scrl & 128 )
+      for ( o= 0x2e000
+          ; o < 0x30000
+          ; o+=4 )
+        eld[o]= bk[0],
+        eld[o+1]= bk[1],
+        eld[o+2]= bk[2];
+    if( scrl & 8 )
+      for ( t= 0
+          , o= -32
+          ; t < 192
+          ; o+= 0x3e0
+          , t++ )
+        for ( v= 0; v<8; v++, o+=4 )
+          eld[o+1024]= bk[0],
+          eld[o+1025]= bk[1],
+          eld[o+1026]= bk[2];
   }
   else
     while( ++t < 0x300 )
