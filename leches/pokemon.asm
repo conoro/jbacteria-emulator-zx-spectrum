9cbb75b8b92be717a0c716b7330f48ba2adf03ad
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2013-02-28 21:46:03 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@339 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- leches/pokemon.asm
+++ leches/pokemon.asm
@@ -171,9 +171,10 @@ pok15   inc     e
         jp      pokm1
 pok16   bit     3, c
         jr      nz, pok08
-        di
         ld      a, (CADEN+1)
         sub     'r'
+        halt
+        di
         ret     z
         ex      af, af'
         ld      c, l
@@ -215,11 +216,11 @@ pok18   ld      c, 11
         ld      (hl), e
         pop     hl
         ld      ($5c78), hl
-        ld      sp, $57e0
-        pop     af
         ex      af, af'
         dec     a
         jr      z, save
+        ld      sp, $57e0
+        pop     af
         pop     iy
         pop     hl
         pop     de
@@ -229,12 +230,30 @@ pok18   ld      c, 11
         retn
 pok19   defb    $ff, $00, $00, $00, $ff, $00, $00, $00, $00, $23, $05
 pok20   defb    $00, $73, $6e, $61, $70, $73, $68, $6f, $74, $34
-        defb    $38, $2b, $00, $0a, $00, $2b, $00
+        defb    $38, $27, $00, $0a, $00, $27, $00
+pok21   defb    $3e, 0,   $ed, $47, $de, $c0, $37, $0e, $8f, $39
+        defb    $96, $01, 0,   0,   $11, 0,   0,   $21, 0,   0
+        defb    $d9, $ed, $56, $fd, $21, 0,   0,   $11, $00, $c0
+        defb    $21, $00, $40, $31, $00, $58, $c3, $f4, $07
+pok22   defb    $21, 0,   0,   $e5, $f1, $08, $01, 0,   0,   $11
+        defb    0,   0,   $dd, $21, 0,   0,   $21, 0,   0,   $e5
+        defb    $f1, $21, 0,   0,   $31, 0,   0,   $f3, $ed, $45
+
+;56e2   $21, 0,   0,   $e5, $f1, $08      af'
+;56e8   $01, 0,   0                       bc
+;56eb   $11, 0,   0                       de
+;56ee   $dd, $21, 0,   0                  ix
+;56f2   $21, 0,   0,   $e5, $f1           af
+;56f7   $21, 0,   0                       hl
+;56fa   $31, 0,   0                       sp
+;56fd   $f3                               iff
+;56fe   $ed, $45
+
 ;00 6D 61 72 69 6F 20 20 20 20 20 2B 00 0A 00 2B 00
 ;57fb-57ff  string
-;57fa       string length
+;57fa       string length 5+1          56e2
 ;57f8       sp
-;57ed-57f7  keyboard variables
+;57ed-57f7  keyboard variables 11
 ;57ec       unused
 ;57ea       af
 ;57e8       bc
@@ -242,7 +261,7 @@ pok20   defb    $00, $73, $6e, $61, $70, $73, $68, $6f, $74, $34
 ;57e4       hl
 ;57e2       iy
 ;57e0       af'
-;56fe       FRAMES1           ix
+;56fe       FRAMES1
 ;56fc       ATTR_T MASK-T     
 ;56fa       I P_FLAG
 ;56f8       FLAGS MODE
@@ -257,20 +276,73 @@ pok20   defb    $00, $73, $6e, $61, $70, $73, $68, $6f, $74, $34
 ;56e6       push hl
 ;56e4       push bc
 ;56e2       push de
-;56e0       call 02bf  
+;56e0       call 02bf    56e2 bloque2
 ;56de       call 028e
 ;56d9                         bloque1
+;56d5                         
 ; o mover de 57b9 a 56d9 (39 bytes)
 ; 3e xx ed 47 de c0 37 0e 8f 39 96 01 xx xx 11 xx xx 21 xx xx d9 ed 56/5e
 ;     i iy                            bc'      de'      hl'         im
 ; fd 21 xx xx 11 00 c0 21 00 40 31 08 80 c3 f4 07  (39 bytes)
 
-save    ld      ($56fe), ix
+save    ld      sp, $5800
+        ld      ($56ef), ix
         ld      ix, pok20
         ld      de, $0011
-        call    $04c2
-        
-pokef   ld      a, 
+        call    $04c6
+        ld      hl, pok21
+        ld      de, $56d9
+        push    de
+        ld      c, $27 ;push bc
+        ldir
+        ld      a, i
+        ld      ($56da), a
+        exx
+        ld      ($56e5), bc
+        ld      ($56e8), de
+        ld      ($56eb), hl
+        ld      a, ($57ec)
+        dec     a
+        ld      a, $5e
+        jr      nz, sav01
+        ld      ($56ef), a
+sav01   ld      hl, ($57e2)
+        ld      ($56f2), hl
+        ld      de, $0027 ;pop de
+        pop     ix
+        ld      a, $ff
+        call    $04c6
+        ld      de, $56e2
+        push    de
+        ld      hl, $05cd
+        push    hl
+        ld      hl, pok22
+        ld      c, $1e
+        ldir
+        ld      sp, $57e0
+        pop     hl
+        ld      ($56e3), hl   ;af'
+        pop     hl
+        pop     hl
+        ld      ($56f8), hl   ;hl
+        pop     hl
+        ld      ($56ec), hl   ;de
+        pop     hl
+        ld      ($56e9), hl   ;bc
+        pop     hl
+        ld      ($56f3), hl   ;af
+        ld      hl, ($57f8)
+        ld      a, i
+        jp      pe, sav02
+        ld      a, $fb
+        ld      ($56fd), a    ;iff
+sav02   ld      ($56fb), hl   ;sp
+        ld      ix, $4000
+        ld      de, $c000
+        ld      a, $ff
+        call    $04c6
+        halt
+pokef
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -280,5 +352,6 @@ pokef   ld      a,
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-        defw    $3cde, 3
+        defw    $3cde, 6
+        ld      ($57ec), a
         jp      $0038
