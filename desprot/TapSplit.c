6499d05e28b8940c816e6ea544b71a09c0f10186
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2013-03-06 10:36:21 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@361 88e056c0-0935-4f5d-8a44-a8e7373e37e9

---
+++ desprot/TapSplit.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+  unsigned char *mem= (unsigned char *) malloc (0x10000);
+  unsigned char tmp;
+  FILE *fi, *fo;
+  long size, size_hi, size_lo;
+  if( argc==1 )
+    printf("\nTapSplit v0.99, a TAP file extractor by Antonio Villena, 6 Mar 2013\n\n"),
+    printf("  TapSplit <input_file> [<output_file>]\n\n"),
+    printf("  <input_file>   Origin file\n"),
+    printf("  <output_file>  Genetated output, stdout if ommited\n"),
+    exit(0);
+  if( argc!=3 )
+    printf("\nInvalid number of parameters\n"),
+    exit(-1);
+  fi= fopen(argv[1], "rb");
+  if( !fi )
+    printf("\nInput file not found: %s\n", argv[1]),
+    exit(-1);
+  fo= fopen(argv[2], "wb+");
+  if( !fo )
+    printf("\nCannot create output file: %s\n", argv[2]),
+    exit(-1);
+  fseek(fi, 0, SEEK_END);
+  size= ftell(fi);
+  size_hi= size>>16;
+  size_lo= size&0xffff;
+  for ( int i= 0; i<size_hi; i++ ){
+    fseek(fi, (-i-1)*0x10000, SEEK_END);
+    fread(mem, 1, 0x10000, fi);
+    for ( int j= 0; j<0x8000; j++ )
+      tmp= mem[j],
+      mem[j]= mem[0xffff-j],
+      mem[0xffff-j]= tmp;
+    fwrite(mem, 1, 0x10000, fo);
+  }
+  rewind(fi);
+  fread(mem, 1, size_lo, fi);
+  for ( int j= 0; j<size_lo>>1; j++ )
+    tmp= mem[j],
+    mem[j]= mem[size_lo-1-j],
+    mem[size_lo-1-j]= tmp;
+  fwrite(mem, 1, size_lo, fo);
+  fclose(fi);
+  fclose(fo);
+  printf("\n0x%X bytes reversed\n", size);
+}
