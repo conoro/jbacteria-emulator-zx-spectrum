387eebcff9623a9bdf43a889c7c253bd706e667d
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2013-10-24 11:55:12 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@452 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- desprot/TmxCnv.c
+++ desprot/TmxCnv.c
@@ -1,59 +1,100 @@
 #include <stdio.h>
 int main(int argc, char* argv[]){
   unsigned char *mem= (unsigned char *) malloc (0x10000);
-  char tmpstr[100];
+  char tmpstr[1000];
+  char *fou;
+  char *token;
   FILE *fi, *fo;
-  int size, scrw, scrh, mapw, maph, lock;
+  int size= 0, scrw, scrh, mapw, maph, lock, numlock= 0, tmpi, elem, sum, tog, i, j, k, l;
   if( argc==1 )
-    printf("\nTmxCnv v0.99, TMX to H generator by Antonio Villena, 23 Oct 2013\n\n"),
-    printf("  TmxCnv <input_f> <output_f>\n\n"),
+    printf("\nTmxCnv v0.99, TMX to H generator by Antonio Villena, 24 Oct 2013\n\n"),
+    printf("  TmxCnv <input_file> <output_file>\n\n"),
     printf("  <input_file>   Origin .TMX file\n"),
     printf("  <output_file>  Generated .H output file\n\n"),
-    printf("Example: TmxCnv map\mapa.tmx dev\mapa.h\n"),
+    printf("Example: TmxCnv map\\mapa.tmx dev\\mapa.h\n"),
     exit(0);
-  if( argc!=8 )
+  if( argc!=3 )
     printf("\nInvalid number of parameters\n"),
     exit(-1);
-  fi= fopen(argv[6], "rb");
+  fi= fopen(argv[1], "r");
   if( !fi )
-    printf("\nInput file not found: %s\n", argv[6]),
+    printf("\nInput file not found: %s\n", argv[1]),
     exit(-1);
-  fo= fopen(argv[7], "wb+");
+  fo= fopen(argv[2], "wb+");
   if( !fo )
-    printf("\nCannot create output file: %s\n", argv[7]),
+    printf("\nCannot create output file: %s\n", argv[2]),
     exit(-1);
-  size= fread(mem, 1, 0x10000, fi);
+  while( !feof(fi) && !strstr(tmpstr, "data e") ){
+    fgets(tmpstr, 1000, fi);
+    if( fou= (char *) strstr(tmpstr, " width") )
+      scrw= atoi(fou+8);
+    if( fou= (char *) strstr(tmpstr, " height") )
+      scrh= atoi(fou+9);
+    if( fou= (char *) strstr(tmpstr, "lock") )
+      lock= atoi(fou+13);
+  }
+  fgets(tmpstr, 1000, fi);
+  token= (char *) strtok(tmpstr, ",");
+  while( token != NULL ){
+    if( tmpi= atoi(token) )
+      mem[size++]= tmpi-1;
+    token= (char *) strtok(NULL, ",");
+  }
+  mapw= scrw-size+1;
+  scrw= size/mapw;
+  fgets(tmpstr, 1000, fi);
+  while( !feof(fi) && !strstr(tmpstr, "map") ){
+    token= (char *) strtok(tmpstr, ",");
+    while( token != NULL ){
+      if( tmpi= atoi(token) )
+        mem[size++]= tmpi-1;
+      token= (char *) strtok(NULL, ",");
+    }
+    fgets(tmpstr, 1000, fi);
+  }
   fclose(fi);
-  mapw= atoi(argv[1]);
-  maph= atoi(argv[2]);
-  scrw= atoi(argv[3]);
-  scrh= atoi(argv[4]);
-  lock= atoi(argv[5]);
-  sprintf(tmpstr, "width=\"%d\" height=\"%d\"", scrw*mapw+mapw-1, scrh*maph+maph-1);
-  fprintf(fo, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-  fprintf(fo, "<map version=\"1.0\" orientation=\"orthogonal\" %s ", tmpstr);
-  fprintf(fo, "tilewidth=\"16\" tileheight=\"16\">\n");
-  fprintf(fo, " <properties><property name=\"lock\" value=\"%d\"/></properties>\n", lock);
-  fprintf(fo, " <tileset firstgid=\"1\" name=\"work\" tilewidth=\"16\" tileheight=\"16\">\n");
-  fprintf(fo, "  <image source=\"../gfx/work.png\"/></tileset>\n");
-  fprintf(fo, " <layer name=\"work\" %s>\n", tmpstr);
-  fprintf(fo, "  <data encoding=\"csv\">\n");
-  for ( int i= 0; i<size; i++ ){
-    if( !(i%scrw) && i%(mapw*scrw) )
-      fprintf(fo, "00,");
-    if( i && !(i%(mapw*scrw*scrh)) ){
-      for ( int j= 0; j<mapw*scrw+mapw-1; j++ )
-        fprintf(fo, "00,");
+  maph= scrh-size/mapw/scrw+1;
+  scrh= (scrh-maph+1)/maph;
+  tmpi= 0;
+  for ( i= 0; i<size; i++ )
+    if( mem[i]>tmpi )
+      tmpi= mem[i];
+  fprintf(fo, "// %s\n", argv[2]);
+  fprintf(fo, "// Generado por MapCnv de la churrera\n");
+  fprintf(fo, "// Copyleft 2010 The Mojon Twins/Antonio Villena\n\n");
+  fprintf(fo, "unsigned char mapa [] = {");
+  for ( i= 0; i<maph; i++ ){
+    fprintf(fo, "\n");
+    for ( j= 0; j<mapw; j++ ){
+      fprintf(fo, "    ");
+      for ( k= 0, sum= 0, tog= 0; k<scrh; k++ )
+        for ( l= 0; l<scrw; l++ ){
+          elem= mem[i*mapw*scrh*scrw+j*scrw+k*mapw*scrw+l];
+          if( lock==elem )
+            numlock++;
+          if( tog^= 1 )
+            sum= elem;
+          else
+            fprintf(fo, "%d, ", sum<<4 | elem);
+        }
       fprintf(fo, "\n");
     }
-    if( i==size-1 )
-      fprintf(fo, "%02d\n", mem[i]+1);
-    else if( (i+1)%(scrw*mapw) )
-      fprintf(fo, "%02d,", mem[i]+1);
-    else
-      fprintf(fo, "%02d,\n", mem[i]+1);
   }
-  fprintf(fo, "</data></layer></map>\n");
+  fprintf(fo, "};\n\n#define MAX_CERROJOS %d\n\n", numlock);
+  fprintf(fo, "typedef struct {\n");
+  fprintf(fo, "    unsigned char np, x, y, st;\n");
+  fprintf(fo, "} CERROJOS;\n\n");
+  fprintf(fo, "CERROJOS cerrojos [MAX_CERROJOS] = {");
+  for ( i= 0; i<maph; i++ )
+    for ( j= 0; j<mapw; j++ )
+      for ( k= 0; k<scrh; k++ )
+        for ( l= 0; l<scrw; l++ )
+          if( lock==mem[i*mapw*scrh*scrw+j*scrw+k*mapw*scrw+l] ){
+            fprintf(fo, "\n    {%d, %d, %d, 0}", i*mapw+j, l, k);
+            if( --numlock )
+              fprintf(fo, ",");
+          }
+  fprintf(fo, "\n};\n\n");
   fclose(fo);
   printf("\nFile generated successfully\n");
 }
