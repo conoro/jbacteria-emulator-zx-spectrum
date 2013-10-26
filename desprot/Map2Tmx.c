7448d0caa0cebbe4b6b2195e27dd2319ae2c9a51
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2013-10-26 11:56:17 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@454 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- desprot/Map2Tmx.c
+++ desprot/Map2Tmx.c
@@ -5,27 +5,31 @@ int main(int argc, char* argv[]){
   FILE *fi, *fo;
   int size, scrw, scrh, mapw, maph, lock;
   if( argc==1 )
-    printf("\nMap2Tmx v0.99, MAP file (Mappy) to TMX (Tiled) by Antonio Villena, 23 Oct 2013\n\n"),
-    printf("  Map2Tmx <map_w> <map_h> <screen_w> <screen_h> <lock> <input_f> <output_f>\n\n"),
-    printf("  <map_w>     Map width\n"),
-    printf("  <map_h>     Map height\n"),
-    printf("  <screen_w>  Screen width\n"),
-    printf("  <screen_h>  Screen height\n"),
-    printf("  <lock>      Tile number of the lock, normally 15\n"),
-    printf("  <input_f>   Origin file\n"),
-    printf("  <output_f>  Generated output file\n\n"),
-    printf("Example: Map2Tmx 5 4 15 10 15 trabajobasura.map mapa.tmx\n"),
+    printf("\nMap2Tmx v1.00, MAP file (Mappy) to TMX (Tiled) by Antonio Villena, 26 Oct 2013\n\n"),
+    printf("  Map2Tmx       <map_width> <map_height> <screen_width> <screen_height>\n"),
+    printf("                <lock> <output_tmx> [<input_map>] [<input_ene>]\n\n"),
+    printf("  <map_width>       Map width\n"),
+    printf("  <map_height>      Map height\n"),
+    printf("  <screen_width>    Screen width\n"),
+    printf("  <screen_height>   Screen height\n"),
+    printf("  <lock>            Tile number of the lock, normally 15\n"),
+    printf("  <output_tmx>      Generated output file\n"),
+    printf("  <input_map>       Origin .map file\n"),
+    printf("  <input_ene>       Origin .ene file\n\n"),
+    printf("Last 2 params are optionally. If not specified will create a black .tmx.\n"),
+    printf("If only the map is specified, the .tmx will have a blank object layer.\n\n"),
+    printf("Example: Map2Tmx 5 4 15 10 15 mapa.tmx trabajobasura.map enems.ene\n"),
     exit(0);
   if( argc!=8 )
     printf("\nInvalid number of parameters\n"),
     exit(-1);
-  fi= fopen(argv[6], "rb");
-  if( !fi )
-    printf("\nInput file not found: %s\n", argv[6]),
-    exit(-1);
-  fo= fopen(argv[7], "wb+");
+  fo= fopen(argv[6], "wb+");
   if( !fo )
-    printf("\nCannot create output file: %s\n", argv[7]),
+    printf("\nCannot create output file: %s\n", argv[6]),
+    exit(-1);
+  fi= fopen(argv[7], "rb");
+  if( !fi )
+    printf("\nInput file not found: %s\n", argv[7]),
     exit(-1);
   size= fread(mem, 1, 0x10000, fi);
   fclose(fi);
@@ -41,7 +45,9 @@ int main(int argc, char* argv[]){
   fprintf(fo, " <properties><property name=\"lock\" value=\"%d\"/></properties>\n", lock);
   fprintf(fo, " <tileset firstgid=\"1\" name=\"work\" tilewidth=\"16\" tileheight=\"16\">\n");
   fprintf(fo, "  <image source=\"../gfx/work.png\"/></tileset>\n");
-  fprintf(fo, " <layer name=\"work\" %s>\n", tmpstr);
+  fprintf(fo, " <tileset firstgid=\"101\" name=\"sprites\" tilewidth=\"16\" tileheight=\"16\">\n");
+  fprintf(fo, "  <image source=\"../gfx/sprites.png\"/></tileset>\n");
+  fprintf(fo, " <layer name=\"map\" %s>\n", tmpstr);
   fprintf(fo, "  <data encoding=\"csv\">\n");
   for ( int i= 0; i<size; i++ ){
     if( !(i%scrw) && i%(mapw*scrw) )
@@ -58,7 +64,9 @@ int main(int argc, char* argv[]){
     else
       fprintf(fo, "%02d,\n", mem[i]+1);
   }
-  fprintf(fo, "</data></layer></map>\n");
+  fprintf(fo, "</data></layer>");
+  fprintf(fo, " <objectgroup name=\"enems\" %s>\n", tmpstr);
+  fprintf(fo, "</objectgroup></map>\n");
   fclose(fo);
   printf("\nFile generated successfully\n");
 }
