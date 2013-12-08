1ad18247e5e4927120dce57d7589fbc76c52b37f
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2013-12-08 16:44:55 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@526 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- buñuelera/engine48.asm
+++ buñuelera/engine48.asm
@@ -6,37 +6,6 @@
         DEFINE  DMAP_BITHALF 1        ; BITSYMB and BITHALF declares 5.5 bits per symbol (16 tiles with 5 bits and 32 with 6 bits)
         DEFINE  DMAP_BUFFER  $5b01    ; BUFFER points to where is decoded the uncompressed screen
 
-; This macro copies a line from immediate value (data is embed in code) to
-; the stack, that correspond with a line (32 bytes) in the screen memory
-
-    MACRO   copy  to
-        ld      sp, $401c+to*16
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-        ld      hl, 0
-        push    hl
-    ENDM
-
 ; This macro multiplies two 8 bits numbers (second one is a constant)
 ; Factor 1 is on E register, Factor 2 is the constant data (macro parameter)
 ; Result is returned on HL
@@ -78,7 +47,7 @@
     ENDM
 
 ; Paolo Ferraris' shortest loader, then we move all the code to $8000
-        output  juego.bin
+        output  engine48.bin
         org     $8000-22
 ini     ld      de, $8000+fin-empe-1
         di
@@ -114,11 +83,9 @@ bucl    ld      a, (y)
 ; Pass the calculated actual screen (from 0 to 23) to the decompressor (after this we have the actual screen at $5801)
         call    descom
 
-; Put the centered initial position (in attribute area) into attr variable because we haven't free registers
-        ld      hl, $5810-scrw
-        ld      (attr), hl
-; Points to the data (in reality is code because is embed) where we paint the tiles
-        ld      hl, screen+12*4
+; Points to screen point where we paint the tiles
+        ld      hl, $4010-scrw
+        ld      bc, $5810-scrw
         exx
 ; BC points to the uncompressed buffer
         ld      bc, DMAP_BUFFER 
@@ -145,98 +112,98 @@ paint2  ld      h, b
         ld      sp, hl
 ; Now SP points to the 36 bytes of the tile that we must to print
         exx
-        ld      bc, 51
 ; Prints the first cell (there are 4 cells to print)
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        ld      de, -357+1
+        ld      de, $f901
         add     hl, de
 ; Prints the second cell 
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        ld      de, 51-1
+        ld      de, $f91f
         add     hl, de
 ; Prints the third cell 
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        ld      de, -357+1
+        ld      de, $f901
         add     hl, de
 ; Prints the fourth cell 
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        add     hl, bc
+        inc     h
         pop     de
         ld      (hl), e
-        add     hl, bc
+        inc     h
         ld      (hl), d
-        ld      de, -765-5
+        ld      de, $f8e1
         add     hl, de
         ex      de, hl
 ; Now we must print the 4 bytes of the attributes
-        ld      hl, (attr)
+        ld      h, b
+        ld      l, c
         pop     bc
         ld      (hl), c
         inc     l
@@ -250,7 +217,8 @@ paint2  ld      h, b
 ; This code updates the attr pointer to the next position
         ld      bc, $ffe1
         add     hl, bc
-        ld      (attr), hl
+        ld      b, h
+        ld      c, l
         ex      de, hl
         exx
         inc     bc
@@ -262,12 +230,17 @@ paint2  ld      h, b
 ; When a line of tiles is printed, the attr pointer must point to the first row on the next line
         ld      bc, $40-(scrw*2)
         add     hl, bc
-        ld      (attr), hl
+        ld      b, h
+        ld      c, l
         ex      de, hl
 ; Do the same with the other pointer
-        ld      de, 816+48
+        ld      de, $40-(scrw*2)
         add     hl, de
-        exx
+        bit     0, h
+        jr      z, pain25
+        ld      de, $0700
+        add     hl, de
+pain25  exx
         ex      af, af'
 ; Repeat 8 times (8 lines of tiles)
         dec     a
@@ -278,199 +251,9 @@ paint2  ld      h, b
 repet   in      a, ($ff)
         inc     a
         jr      z, repet
-; Now we generate the playing area of pre-generated tiles to erase the sprites
-; This is not visible because speed of generation is slower than the electron beam
-; We must generate the lines in the same non linear order than the electrom beam (this takes about 40000 cycles)
-screen  copy    $00
-        copy    $10
-        copy    $20
-        copy    $30
-        copy    $40
-        copy    $50
-        copy    $60
-        copy    $70
-        copy    $02
-        copy    $12
-        copy    $22
-        copy    $32
-        copy    $42
-        copy    $52
-        copy    $62
-        copy    $72
-        copy    $04
-        copy    $14
-        copy    $24
-        copy    $34
-        copy    $44
-        copy    $54
-        copy    $64
-        copy    $74
-        copy    $06
-        copy    $16
-        copy    $26
-        copy    $36
-        copy    $46
-        copy    $56
-        copy    $66
-        copy    $76
-        copy    $08
-        copy    $18
-        copy    $28
-        copy    $38
-        copy    $48
-        copy    $58
-        copy    $68
-        copy    $78
-        copy    $0a
-        copy    $1a
-        copy    $2a
-        copy    $3a
-        copy    $4a
-        copy    $5a
-        copy    $6a
-        copy    $7a
-        copy    $0c
-        copy    $1c
-        copy    $2c
-        copy    $3c
-        copy    $4c
-        copy    $5c
-        copy    $6c
-        copy    $7c
-        copy    $0e
-        copy    $1e
-        copy    $2e
-        copy    $3e
-        copy    $4e
-        copy    $5e
-        copy    $6e
-        copy    $7e
-        copy    $80
-        copy    $90
-        copy    $a0
-        copy    $b0
-        copy    $c0
-        copy    $d0
-        copy    $e0
-        copy    $f0
-        copy    $82
-        copy    $92
-        copy    $a2
-        copy    $b2
-        copy    $c2
-        copy    $d2
-        copy    $e2
-        copy    $f2
-        copy    $84
-        copy    $94
-        copy    $a4
-        copy    $b4
-        copy    $c4
-        copy    $d4
-        copy    $e4
-        copy    $f4
-        copy    $86
-        copy    $96
-        copy    $a6
-        copy    $b6
-        copy    $c6
-        copy    $d6
-        copy    $e6
-        copy    $f6
-        copy    $88
-        copy    $98
-        copy    $a8
-        copy    $b8
-        copy    $c8
-        copy    $d8
-        copy    $e8
-        copy    $f8
-        copy    $8a
-        copy    $9a
-        copy    $aa
-        copy    $ba
-        copy    $ca
-        copy    $da
-        copy    $ea
-        copy    $fa
-        copy    $8c
-        copy    $9c
-        copy    $ac
-        copy    $bc
-        copy    $cc
-        copy    $dc
-        copy    $ec
-        copy    $fc
-        copy    $8e
-        copy    $9e
-        copy    $ae
-        copy    $be
-        copy    $ce
-        copy    $de
-        copy    $ee
-        copy    $fe
+
 ; Restores the stack, we need it for do CALLs
 paint3  ld      sp, 0
-; We will paint 12 enemies, storing the actual value in sprind (variable embed into code)
-        ld      a, 11
-busp    ld      (sprind+1), a
-; Point HL to the parameters (4 bytes) of actual enemy
-        add     a, a
-        add     a, a
-        ld      l, a
-        ld      h, ene0 >> 8
-; Reads X and Y position
-        ld      c, (hl)
-        inc     l
-        ld      b, (hl)
-        inc     l
-; Test vertical direction
-        bit     0, (hl)
-        jr      nz, binc
-; If direction is up, decrement Y and test upper edge
-        dec     b
-        djnz    bfin
-; If upper edge detected invert vertical direction
-        inc     (hl)
-        jr      bfin
-; If direction is down, increment Y, test and process lower edge
-binc    inc     b
-        inc     b
-        ld      a, $70
-        cp      b
-        jr      nz, bfin
-        dec     (hl)
-; Do the same in horizontal direction
-bfin    bit     1, (hl)
-        jr      nz, cinc
-        dec     c
-        dec     c
-        ld      a, $1e
-        cp      c
-        jr      nz, cfin
-        set     1, (hl)
-        jr      cfin
-cinc    inc     c
-        inc     c
-        ld      a, $d2
-        cp      c
-        jr      nz, cfin
-        res     1, (hl)
-; Read sprite picture to use in A
-cfin    inc     l
-        ld      a, (hl)
-        dec     l
-        dec     l
-; Update X and Y to memory
-        ld      (hl), b
-        dec     l
-        ld      (hl), c
-; Paint the enemy sprite
-        call    put_sprite
-; Repeat 12 times
-sprind  ld      a, 0
-        dec     a
-        jp      p, busp
 ; Paint the main character sprite
         ld      bc, (corx)
         xor     a
@@ -680,7 +463,6 @@ key3    ld      (hl), 0
         ret
 
 ; Some variables
-attr    dw      $5810-scrw
 x       db      0
 y       db      0
 corx    db      32
---
+++ buñuelera/engine48.bat
@@ -0,0 +1,8 @@
+rem TableGen
+rem TileGen
+rem TmxCompress map.tmx map_compressed.bin
+\_Downloads\dm\bin\dmc tilegen.c lodepng.c
+GfxBu sprites.png sprites.bin 9e00
+\emuscriptoria\sjasmplus engine48.asm
+..\desprot\gentape engine48.tap basic 'engine48' 0 engine48.bin
+engine48.tap
