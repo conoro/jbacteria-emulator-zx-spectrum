d48b6ea8e0a296df155abc76883f6176940e7d88
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2014-08-17 01:13:51 +0000

    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@646 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- unkatris.asm
+++ unkatris.asm
@@ -1,47 +1,7 @@
         output  unkatris.p
-        define  _SP     $00
-        define  _0      $1c
-        define  _1      _0+1
-        define  _2      _1+1
-        define  _3      _2+1
-        define  _4      _3+1
-        define  _5      _4+1
-        define  _6      _5+1
-        define  _7      _6+1
-        define  _8      _7+1
-        define  _9      _8+1
-        define  _A      _9+1
-        define  _B      _A+1
-        define  _C      _B+1
-        define  _D      _C+1
-        define  _E      _D+1
-        define  _F      _E+1
-        define  _G      _F+1
-        define  _H      _G+1
-        define  _I      _H+1
-        define  _J      _I+1
-        define  _K      _J+1
-        define  _L      _K+1
-        define  _M      _L+1
-        define  _N      _M+1
-        define  _O      _N+1
-        define  _P      _O+1
-        define  _Q      _P+1
-        define  _R      _Q+1
-        define  _S      _R+1
-        define  _T      _S+1
-        define  _U      _T+1
-        define  _V      _U+1
-        define  _W      _V+1
-        define  _X      _W+1
-        define  _Y      _X+1
-        define  _Z      _Y+1
-        define  _NL     $76
-
         org     $4009
 
-VERSN   defb    1, 0
-        defb    $12
+VERSN   defb    1, 0, 0
 D_FILE  defw    dfile
 
 inic    call    $02bb
@@ -125,13 +85,13 @@ opc2    ld      a, (de)         ; Leo el color (en A) de la posición actual (de
 inca    dec     h
         ld      (score+6-2), hl
         ld      (score+6-4), hl
-        ld      hl, map-1
+        ld      hl, dfile-1
         ld      (desc+1), hl
         ld      (iy+3), 13
 
 nxtl    ld      de, screen+11*20-1
-desc    ld      hl, map-1
-desc1   ld      bc, $8000       ; marker bit
+desc    ld      hl, 0
+desc1   ld      bc, 0           ; marker bit
 desc2   xor     a
         call    gbit            ; load bitsym bits (literal)
         add     a, 7
@@ -148,7 +108,7 @@ desc5   dec     e               ; test end of file (map is always 150 bytes)
         call    gbit
         jr      z, desc8        ; 010 = -11
         call    gbitd           ; [011, 100, 101, 110, 111] xx
-        sub     13
+        sub     0
         jr      z, desc5
         ccf
 desc8   adc     a, 10
@@ -210,7 +170,7 @@ tli1    ex      de, hl          ; Almaceno posición en DE
         jr      z, tli1         ; Bucle que voy repitiendo (probando con líneas que están por encima) hasta detectar una falsa línea completa en la ROM (debajo de $4000)
         ld      c, l
         bit     0, h
-        jr      nz, nnwp         ; Si me salgo de la zona de atributos es que ya he llegado a la primera línea y por tanto salgo del bucle (a generar una nueva pieza)
+        jr      nz, nnwp        ; Si me salgo de la zona de atributos es que ya he llegado a la primera línea y por tanto salgo del bucle (a generar una nueva pieza)
         ld      d, h
         sbc     hl, hl
         ld      e, h
@@ -226,7 +186,7 @@ rand    ld      a, (FRAMES)
         ld      (VERSN), hl
         ld      hl, kaka
         push    hl
-        ld      de, screen+1+2*11+6 ;& 255 ; La primera pieza parte de la coordenada (x, y)= (6, 2), en el registro DE guardo la posición
+        ld      de, screen+1+2*11+6 ; La primera pieza parte de la coordenada (x, y)= (6, 2), en el registro DE guardo la posición
 leep    add     a, tabla-1&255
         ld      l, a            ; Paso dicho valor al registro L para leer pieza de la tabla
         add     a, 156-tabla+1&255
@@ -301,7 +261,7 @@ salt    dec     a
         jr      c, nizq         ; No, pues salto y no hago nada
         dec     e               ; Sí, pues decremento posición
 nizq    rrca
-        jr      c, nder        ; No, pues salto y no hago nada
+        jr      c, nder         ; No, pues salto y no hago nada
         inc     e               ; Sí, pues incremento posición
 nder    sub     $fd
         ld      c, 1            ; Inicializo A y C a cero y uno respectivamente, independientemente de si salto o no
@@ -338,39 +298,36 @@ rot1    add     hl, hl          ; Desplazo 4 veces HL a la izquierda
 
         incbin  unkatris.bin
 map
-
         block   $4300-44-$, $fe
 
-dfile   defb    _NL,49,42,59,42,49,0,135,131,131,4
-level   defb    _NL,0,0,0,0,0,0,133,30,28,5
-        defb    _NL,56,40,52,55,42,0,133,29,32,5
-score   defb    _NL,0,0,0,0,0,28,2,3,3,1
-screen  defb    _NL,132,4,0,27,0,5,0,0,132,1
-        defb    _NL,131,0,0,0,0,5,27,0,135,129
-        defb    _NL,5,0,27,0,0,5,0,27,0,131
-        defb    _NL,4,0,0,27,133,132,27,0,0,3
-        defb    _NL,132,27,0,0,129,133,131,131,0,133
-        defb    _NL,0,0,135,0,5,0,0,133,0,133
-        defb    _NL,27,0,133,0,5,133,0,129,4,27
-        defb    _NL,0,0,133,3,1,129,4,5,5,0
-        defb    _NL,0,0,7,5,0,5,5,1,132,0
-        defb    _NL,0,7,5,5,0,5,5,0,133,0
-        defb    _NL,133,1,1,1,0,0,0,0,2,5
-        defb    _NL,2,137,137,137,137,137,137,137,137,1
-        defb    _NL,0,4,4,4,135,135,135,135,131,0
-        defb    _NL,0,5,5,7,129,133,6,133,129,0
-        defb    _NL,0,130,5,5,133,133,133,133,133,0
-        defb    _NL,0,135,131,0,131,135,135,131,0,0
-        defb    _NL,136,0,5,2,129,133,133,131,0,136
-        defb    _NL,136,0,5,133,133,133,135,129,0,136
-        defb    _NL,39,62,0,38,51,57,52,51,46,52
-        defb    _NL,22,22,0,59,46,49,49,42,51,38
-        defb    _NL
+dfile   defb    $76,49,42,59,42,49,0,135,131,131,4
+level   defb    $76,0,0,0,0,0,0,133,30,28,5
+        defb    $76,56,40,52,55,42,0,133,29,32,5
+score   defb    $76,0,0,0,0,0,28,2,3,3,1
+screen  defb    $76,132,4,0,27,0,5,0,0,132,1
+        defb    $76,131,0,0,0,0,5,27,0,135,129
+        defb    $76,5,0,27,0,0,5,0,27,0,131
+        defb    $76,4,0,0,27,133,132,27,0,0,3
+        defb    $76,132,27,0,0,129,133,131,131,0,133
+        defb    $76,0,0,135,0,5,0,0,133,0,133
+        defb    $76,27,0,133,0,5,133,0,129,4,27
+        defb    $76,0,0,133,3,1,129,4,5,5,0
+        defb    $76,0,0,7,5,0,5,5,1,132,0
+        defb    $76,0,7,5,5,0,5,5,0,133,0
+        defb    $76,133,1,1,1,0,0,0,0,2,5
+        defb    $76,2,137,137,137,137,137,137,137,137,1
+        defb    $76,0,4,4,4,135,135,135,135,131,0
+        defb    $76,0,5,5,7,129,133,6,133,129,0
+        defb    $76,0,130,5,5,133,133,133,133,133,0
+        defb    $76,0,135,131,0,131,135,135,131,0,0
+        defb    $76,136,0,5,2,129,133,133,131,0,136
+        defb    $76,136,0,5,133,133,133,135,129,0,136
+        defb    $76,39,62,0,38,51,57,52,51,46,52
+        defb    $76,22,22,0,59,46,49,49,42,51,38
+        defb    $76
 
         block   $43fc-$
         defw    inic
-
-; Start of the variables area used by BASIC.
         nop
         defb    $80
 eline
