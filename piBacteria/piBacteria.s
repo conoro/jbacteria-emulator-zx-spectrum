16a37b5f07760cd58f55f5ce853244100aa8a0c4
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2014-10-31 16:32:09 +0000

    Añado funcionalidad en revisión 1.0 de la Raspberry
    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@712 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- piBacteria/piBacteria.s
+++ piBacteria/piBacteria.s
@@ -54,20 +54,29 @@
         ldr     mem, memo
 
 @ Esto es para configurar el buffer de video a 352x264x4
+        ldr     r2, mboxb
+        add     r0, mem, #ogetrev+8
+        mov     r4, #8
+        bl      mbox
+        ldr     r3, [mem, #ogetrev+20]
+        cmp     r3, #4
+        bcs     nrev1
+        ldr     r3, _table
+        add     r3, #80
+        add     r0, r3, #68
+        ldmia   r0!, {r4-r11}
+        stmia   r3!, {r4-r11}
+        ldmia   r0!, {r4-r12}
+        stmia   r3!, {r4-r12}
+        mov     r0, #0x23
+        strb    r0, parch1
+        strb    r0, parch2
+        mov     r0, #0x37
+        strb    r0, parch2+1
+nrev1:  mov     r4, #1
         add     r0, mem, #ofbinfo+1
         orr     r0, #0x40000000
-        ldr     r2, mboxb
-wait1:  ldr     r3, [r2, #MBOXSTATUS]
-        tst     r3, #0x80000000
-        bne     wait1
-        str     r0, [r2, #MBOXWRITE]
-wait2:  ldr     r3, [r2, #MBOXSTATUS]
-        tst     r3, #0x40000000
-        bne     wait2
-        ldr     r3, [r2, #MBOXREAD]
-        and     r3, #0x0000000f
-        cmp     r3, #1
-        bne     wait2
+        bl      mbox
 
 @ Pongo a cero las filas y a pull up las columnas (y el puerto EAR)
         ldr     r0, gpbas
@@ -172,7 +181,7 @@ drawp:  sub     r11, r3, #6
         add     r11, r11, lr, lsl #8
         ldr     r12, [mem, #opinrap]
         ldr     r11, [r12, r11, lsl #2]
-aqui:   ldrcs   r11, [mem, #oborder]
+aqui:   ldrcs   r11, border
         str     r11, [r10], #4
         add     r3, #1
         cmp     r3, #44
@@ -243,7 +252,6 @@ irqhnd: push    {r0, r1}
         str     r1, [r0, #STC1]
         pop     {r0, r1}
         subs    pc, lr, #4
-
 
   .if debug==1
 regs:   push    {r0, r12, lr}
@@ -341,15 +349,14 @@ stbas:  .word   STBASE
 intbas: .word   INTBASE
 irqh:   .word   irqhnd-0x20
 memo:   .word   MEMORY
-rows:   .word   0b00001000010000100000111000011000
-filt:   .word   0b00000011100000000000000110000100
 _table: .word   table
-table:  .word   0b001000000000000001001000000000
-        .word   0b001000000000000000001000000000
+rows:   .word   0b00001000011000100000111000011010
+table:  .word   0b001000000000000001001000001000
+        .word   0b001000000000000000001000001000
         .word   0b001000000000000001000000000000
         .word   0b001000000000000000000000000000
-        .word   0b000000000000000001001000000000
-        .word   0b000000000000000000001000000000
+        .word   0b000000000000000001001000001000
+        .word   0b000000000000000000001000001000
         .word   0b000000000000000001000000000000
         .word   0b000000000000000000000000000000
         .word   0b000000001000010010000000001001
@@ -360,10 +367,11 @@ table:  .word   0b001000000000000001001000000000
         .word   0b000000001000010010000000000000
         .word   0b000000000000010010000000001000
         .word   0b000000000000010010000000000000
-        .word   0b000000001000000000000001000000
-        .word   0b000000001000000000000000000000
+        .word   0b000000001000000000000001001000
+        .word   0b000000001000000000000000001000
         .word   0b000000000000000000000001000000
         .word   0b000000000000000000000000000000
+filt:   .word   0b00000011100000000000000110000100
         .byte   0b10100000
         .byte   0b10110000
         .byte   0b10101000
@@ -428,6 +436,72 @@ table:  .word   0b001000000000000001001000000000
         .byte   0b11110111
         .byte   0b11101111
         .byte   0b11111111
+        .word   0b00000011100000000000000110000001
+        .byte   0b10100000
+        .byte   0b11100000
+        .byte   0b10110000
+        .byte   0b11110000
+        .byte   0b10101000
+        .byte   0b11101000
+        .byte   0b10111000
+        .byte   0b11111000
+        .byte   0b10100001
+        .byte   0b11100001
+        .byte   0b10110001
+        .byte   0b11110001
+        .byte   0b10101001
+        .byte   0b11101001
+        .byte   0b10111001
+        .byte   0b11111001
+        .byte   0b10100010
+        .byte   0b11100010
+        .byte   0b10110010
+        .byte   0b11110010
+        .byte   0b10101010
+        .byte   0b11101010
+        .byte   0b10111010
+        .byte   0b11111010
+        .byte   0b10100011
+        .byte   0b11100011
+        .byte   0b10110011
+        .byte   0b11110011
+        .byte   0b10101011
+        .byte   0b11101011
+        .byte   0b10111011
+        .byte   0b11111011
+        .byte   0b10100100
+        .byte   0b11100100
+        .byte   0b10110100
+        .byte   0b11110100
+        .byte   0b10101100
+        .byte   0b11101100
+        .byte   0b10111100
+        .byte   0b11111100
+        .byte   0b10100101
+        .byte   0b11100101
+        .byte   0b10110101
+        .byte   0b11110101
+        .byte   0b10101101
+        .byte   0b11101101
+        .byte   0b10111101
+        .byte   0b11111101
+        .byte   0b10100110
+        .byte   0b11100110
+        .byte   0b10110110
+        .byte   0b11110110
+        .byte   0b10101110
+        .byte   0b11101110
+        .byte   0b10111110
+        .byte   0b11111110
+        .byte   0b10100111
+        .byte   0b11100111
+        .byte   0b10110111
+        .byte   0b11110111
+        .byte   0b10101111
+        .byte   0b11101111
+        .byte   0b10111111
+        .byte   0b11111111
+
 in:     tst     r0, #1
         movne   r0, #0xff
         bxne    lr
@@ -453,10 +527,10 @@ in:     tst     r0, #1
         bl      wait
         pop     {r2, lr}
         ldr     r3, [r2, #GPLEV0]
-        ldr     r0, filt
+        ldr     r0, [r11], #4
         and     r3, r0
-        orr     r3, r3, lsr #7
-        orr     r3, r3, lsr #13
+parch1: orr     r3, r3, lsr #7
+parch2: orr     r3, r3, lsr #13
         and     r3, #0b111111
         ldrb    r0, [r11, r3]
         bx      lr
@@ -467,18 +541,42 @@ out:    tst     r0, #1
         ldr     r0, c1111
         mul     r1, r0, r1
         ldr     r0, memo
-        str     r1, [r0, #oborder]
+        str     r1, border
         bx      lr
 c1111:  .word   0x11111111
+border: .word   0x77777777
 
 wait:   mov     r2, #50
 waita:  subs    r2, #1
         bne     waita
         bx      lr
 
+mbox:   ldr     r3, [r2, #MBOXSTATUS]
+        tst     r3, #0x80000000
+        bne     mbox
+        str     r0, [r2, #MBOXWRITE]
+mbox1:  ldr     r3, [r2, #MBOXSTATUS]
+        tst     r3, #0x40000000
+        bne     mbox1
+        ldr     r3, [r2, #MBOXREAD]
+        and     r3, #0x0000000f
+        cmp     r3, r4
+        bne     mbox1
+        bx      lr
+
         .include  "z80.s"
 
         .balign 16
+
+getrev: .word   7*4
+        .word   0
+        .word   0x00010002
+        .word   4
+        .word   0
+        .word   0
+        .word   0
+        .word   0
+
 fbinfo: .word   1024    @0 Width
         .word   768     @4 Height
         .word   352     @8 vWidth
@@ -510,7 +608,6 @@ point:  .word   0       @32 GPU - Pointer
 pinrap: .word   LTABLE
 tmpr2:  .word   0
 tmpr3:  .word   0
-border: .word   0x77777777
         .byte   0, 0, 0
 a_:     .byte   0
 fa_:    .short  0
@@ -529,12 +626,12 @@ h_:     .byte   0
         .equ    off_,     oc_-4
         .equ    ofa_,     off_-4
         .equ    oa_,      ofa_-1
-        .equ    oborder,  oa_-7
-        .equ    otmpr3,   oborder-4
+        .equ    otmpr3,   oa_-7
         .equ    otmpr2,   otmpr3-4
         .equ    opinrap,  otmpr2-4
         .equ    opoint,   opinrap-40
         .equ    ofbinfo,  opoint-32
+        .equ    ogetrev,  ofbinfo-32
 
 endf:
 @        .incbin "ManicMiner.rom"
