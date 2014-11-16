ad01fa225b4274c207a01a99700d2dff917f0db1
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2014-11-16 22:55:09 +0000

    actualizo bootloader
    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@725 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- piBacteria/bootloader.s
+++ piBacteria/bootloader.s
@@ -1,3 +1,4 @@
+        .set    STCLO,      0x20003004
         .set    AUXBASE,    0x20215000
         .set    AMENABLES,  0x04
         .set    AMIOREG,    0x40
@@ -14,8 +15,11 @@
         block   .req    r3
         addr    .req    r4
         crc     .req    r5
+        counter .req    r6
+        cnt     .req    r7
+        timeout .req    r8
 .text
-start:  mov     addr, #0x8100
+start:  mov     addr, #0x8200
 star1:  ldr     r1, [addr], #-4
         str     r1, [addr, #-4092]
         cmp     addr, #0x8000
@@ -31,46 +35,52 @@ star2:  ldr     gpbas, =GPBASE
         str     r1, [auxbas, #AMLCRREG]
         add     r1, #270-0x23
         str     r1, [auxbas, #AMBAUDREG]
-star3:  ldr     r1, [auxbas, #AMLSRREG]
+        ldr     counter, =STCLO
+        ldr     cnt, [counter]
+star3:  add     timeout, cnt, #0xa000
+        mov     send, #0x15
+        str     send, [auxbas, #AMIOREG]
+star4:  ldr     cnt, [counter]
+        cmp     cnt, timeout
+        bcs     star3
+        ldr     r1, [auxbas, #AMLSRREG]
         tst     r1, #1
-        beq     star3
+        beq     star4
         ldr     recv, [auxbas, #AMIOREG]
+        add     timeout, cnt, #0xa000
         cmp     state, #1
-        bcs     star5
+        bcs     star6
         cmp     recv, #0x01
         moveq   crc, #0
         beq     stard
         cmp     recv, #0x04
-        bne     star7
+        bne     star8
         mov     send, #0x06
         str     send, [auxbas, #AMIOREG]
-star4:  subs    addr, #1
-        bne     star4
+star5:  subs    addr, #1
+        bne     star5
         b       start+0x1000
-star5:  bne     stara
+star6:  bne     stara
         cmp     recv, block
-star6:  beq     stard
-star7:  mov     send, #0x15
-star8:  mov     state, #0
-star9:  ldr     r6, [auxbas, #AMLSRREG]
-        tst     r6, #0x20
-        beq     star9
+star7:  beq     stard
+star8:  mov     send, #0x15
+star9:  mov     state, #0
         str     send, [auxbas, #AMIOREG]
-        b       star3
+        b       star4
 stara:  cmp     state, #2
         bne     starb
-        eor     lr, block, #0xff
-        cmp     recv, lr
-        b       star6
+        eor     cnt, block, #0xff
+        cmp     recv, cnt
+        b       star7
 starb:  cmp     state, #131
         bne     starc
         cmp     recv, crc
-        bne     star7
+        bne     star8
         mov     r1, #1
         uadd8   block, block, r1
         mov     send, #0x06
-        b       star8
+        b       star9
 starc:  uadd8   crc, crc, recv
         strb    recv, [addr], #1
 stard:  add     state, #1
-        b       star3
+        b       star4
