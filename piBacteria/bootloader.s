824bb00d0d4bd1b5bb43337f12973caa19aefdb1
antoniovillena <antoniovillena@88e056c0-0935-4f5d-8a44-a8e7373e37e9>
2014-11-18 01:33:06 +0000

    Dejo el bootloader en 248 bytes
    git-svn-id: http://svn.code.sf.net/p/emuscriptoria/code@727 88e056c0-0935-4f5d-8a44-a8e7373e37e9

--- piBacteria/bootloader.s
+++ piBacteria/bootloader.s
@@ -8,43 +8,44 @@
         .set    GPBASE,     0x20200000
         .set    GPFSEL1,    0x04
         state   .req    r0
-        recv    .req    r1
-        send    .req    r1
-        gpbas   .req    r2
-        auxbas  .req    r2
-        block   .req    r3
-        addr    .req    r4
-        crc     .req    r5
-        counter .req    r6
-        cnt     .req    r7
-        timeout .req    r8
+        recv    .req    r3
+        send    .req    r3
+        gpbas   .req    r4
+        auxbas  .req    r4
+        block   .req    r5
+        addr    .req    r6
+        crc     .req    r7
+        cntadr  .req    r8
+        cnt     .req    r9
+        timeout .req    r10
 .text
-start:  mov     addr, #0x8200
-star1:  ldr     r1, [addr], #-4
-        str     r1, [addr, #-4092]
+start:  mov     addr, #0x8100
+star1:  ldr     r3, [addr], #-4
+        str     r3, [addr, #4-0x100]
         cmp     addr, #0x8000
         bne     star1
-        b       star2-0x1000
-star2:  ldr     gpbas, =GPBASE
-        mov     r1, #0b00000000000000010010000000000000
-        str     r1, [gpbas, #GPFSEL1]
+        ldr     gpbas, =GPBASE
+        sub     cntadr, gpbas, #GPBASE-0x20004000
+        mov     r3, #0b00000000000000010010000000000000
+        str     r3, [gpbas, #GPFSEL1]
         add     auxbas, #AUXBASE-GPBASE
         mov     block, #1
         str     block, [auxbas, #AMENABLES]
-        mov     r1, #0x23
-        str     r1, [auxbas, #AMLCRREG]
-        add     r1, #270-0x23
-        str     r1, [auxbas, #AMBAUDREG]
-        ldr     counter, =STCLO
-        ldr     cnt, [counter]
-star3:  add     timeout, cnt, #0xa000
-        mov     send, #0x15
-        str     send, [auxbas, #AMIOREG]
-star4:  ldr     cnt, [counter]
+        mov     timeout, #0x23
+        str     timeout, [auxbas, #AMLCRREG]
+        add     r3, timeout, #270-0x23
+        str     r3, [auxbas, #AMBAUDREG]
+        b       star4-0x100
+star2:  uadd8   crc, crc, recv
+        strb    recv, [addr], #1
+star3:  add     state, #1
+star4:  ldr     cnt, [cntadr, #STCLO-0x20004000]
         cmp     cnt, timeout
-        bcs     star3
-        ldr     r1, [auxbas, #AMLSRREG]
-        tst     r1, #1
+        addcs   timeout, cnt, #0xa000
+        movcs   send, #0x15
+        strcs   send, [auxbas, #AMIOREG]
+        ldr     r3, [auxbas, #AMLSRREG]
+        tst     r3, #1
         beq     star4
         ldr     recv, [auxbas, #AMIOREG]
         add     timeout, cnt, #0xa000
@@ -52,18 +53,18 @@ star4:  ldr     cnt, [counter]
         bcs     star6
         cmp     recv, #0x01
         moveq   crc, #0
-        beq     starc
+        beq     star3
         cmp     recv, #0x04
         bne     star8
         mov     send, #0x06
         str     send, [auxbas, #AMIOREG]
 star5:  subs    addr, #1
         bne     star5
-        b       start+0x1000
+        b       start+0x100
 star6:  bne     stara
         mov     cnt, block
 star7:  cmp     recv, cnt
-        beq     starc
+        beq     star3
 star8:  mov     send, #0x15
 star9:  mov     state, #0
         str     send, [auxbas, #AMIOREG]
@@ -72,14 +73,10 @@ stara:  cmp     state, #2
         eoreq   cnt, block, #0xff
         beq     star7
         cmp     state, #131
-        bne     starb
+        bne     star2
         cmp     recv, crc
         bne     star8
-        mov     r1, #1
-        uadd8   block, block, r1
+        mov     r3, #1
+        uadd8   block, block, r3
         mov     send, #0x06
         b       star9
-starb:  uadd8   crc, crc, recv
-        strb    recv, [addr], #1
-starc:  add     state, #1
-        b       star4
